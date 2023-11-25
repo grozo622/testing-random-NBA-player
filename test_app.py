e43bf6f47ad7a9a4f0d1d0858430677c7126be3d
grozo622 <45059559+grozo622@users.noreply.github.com>
2023-11-25 20:34:59 +0000

    updating test_app.py to test handling all inputs

--- test_app.py
+++ test_app.py
@@ -1,5 +1,13 @@
-from app import random_nba_player
-
+from app import random_nba_player, get_player_info
 
 def test_random_nba_player():
-    assert "Stephen Curry" or "Lebron James" or "Damian Lillard" in random_nba_player()
+    players = ["Stephen Curry", "Lebron James", "Damian Lillard"]
+    result = random_nba_player()
+    assert result in players
+
+def test_get_player_info():
+    # You may need to customize this test based on the actual behavior of get_player_info
+    player_name = "Stephen Curry"
+    player_image, player_stats = get_player_info(player_name)
+    assert player_image is not None
+    assert player_stats is not None
